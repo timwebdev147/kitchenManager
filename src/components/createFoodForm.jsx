9e5127100f4c147fa72599294d7376557a724dce
Akinsanmi Timothy <timwebdev147@gmail.com>
2022-09-27 10:47:57 -0700

    changes

--- src/components/createFoodForm.jsx
+++ src/components/createFoodForm.jsx
@@ -215,7 +215,7 @@ function    CreateFoodForm(){
     const [formFields, dispatch] = useReducer(reducer, initialFieldsState);
 
     const [condition, setCondition] = useState(false);
-    const [afterSubmit, setAfterSubmit] = useState(false)
+    const [afterSubmit, setAfterSubmit] = useState(true)
     const [emptyAlert, setEmptyAlert] = useState(null);
 
     const updateFieldValue = (index, value ) => {
